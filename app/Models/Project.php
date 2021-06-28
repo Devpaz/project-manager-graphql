187fd2ac0a50ad6e5ba0ad377f0904032a2d029d
Paolo Paz <sl.paolo.paz@gmail.com>
2021-06-28 18:48:58 -0500

    my first commit of the project

---
+++ app/Models/Project.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Project extends Model
+{
+    use HasFactory;
+    protected $fillable =[
+        'title', 'description', 'manager_id'
+    ];
+
+    public function manager(){
+        return $this->belongsTo('App\Models\user', 'manager_id');
+    }
+
+    public function tasks(){
+        return $this->hasMany('App\Models\Task');
+    }
+    public function users(){
+        return $this->belongsToMany('App\Models\User');
+    }
+}
---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use App\Models\User;
+use App\Models\Project;
+use App\Models\Task;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::statement('SET FOREIGN_KEY_CHECKS=0;');
+        DB::table('project_user')->truncate();
+        Project::truncate();
+        Task::truncate();
+        User::truncate();
+        DB::statement('SET FOREIGN_KEY_CHECKS=1;');
+
+        $admin = User::create([
+           'name'=>'Super Admin',
+           'email'=>'paolo@devpaz.com',
+            'password'=>bcrypt('secret')
+        ]);
+        $user = User::create([
+            'name'=>'Paolo Paz',
+            'email'=>'paolo2@devpaz.com',
+            'password'=>bcrypt('secret')
+        ]);
+        $project = Project::create([
+            'title'=>'Create Project Manager App',
+            'description'=>'Write the Project Manager',
+            'manager_id'=>$admin->id
+
+        ]);
+        $task1 = Task::create([
+            'title'=>'Seed Database',
+            'description'=>'seed the database with test data',
+            'status_code'=>'COMP',
+            'project_id'=>$project->id,
+            'user_id'=>$admin->id
+        ]);
+        $task2 = Task::create([
+            'title'=>'Complete Development',
+            'description'=>'Write all the code',
+            'status_code'=>'OPEN',
+            'project_id'=>$project->id,
+            'user_id'=>$user->id
+        ]);
+
+        $project->users()->saveMany([$admin, $user]);
+
+    }
+}
